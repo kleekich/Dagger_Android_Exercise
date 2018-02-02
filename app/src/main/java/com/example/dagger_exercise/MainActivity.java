dab941ed3399350238f01097d03f9f3180a1aaea
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-02-01 21:14:17 -0800

    myApplication class added

--- app/src/main/java/com/example/dagger_exercise/MainActivity.java
+++ app/src/main/java/com/example/dagger_exercise/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.dagger_exercise;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.app.Activity;
+
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjection;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Inject
+        NetworkApi networkApi;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            AndroidInjection.inject(this);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            boolean injected = networkApi == null ? false : true;
+            TextView userAvailable = (TextView) findViewById(R.id.target);
+            userAvailable.setText("Dependency injection worked: " + String.valueOf(injected));
     }
 }
---
+++ app/src/main/java/com/example/dagger_exercise/MyApplication.java
@@ -0,0 +1,14 @@
+package com.example.dagger_exercise;
+
+import android.app.Application;
+
+/**
+ * Created by kangsik_kevin_lee on 2/1/18.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
